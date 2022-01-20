2f74416147b8a5fbc493b51fca511a70930ae5c5
khaledrefaat <khaledrefaat08@gmail.com>
2022-01-20 08:36:33 +0200

    adding starter files for redux asynchronous demo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-
-import { useSelector } from 'react-redux';
-import UserProfile from './components/UserProfile';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const isLoggedIn = useSelector(({ auth }) => auth.isLoggedIn);
-
   return (
-    <>
-      <Header />
-      {isLoggedIn ? <UserProfile /> : <Auth />}
-      <Counter />
-    </>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
